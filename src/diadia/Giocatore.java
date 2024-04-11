e606c37172a3033acb121eba942fdad82aff513d
Fedmecucci <129685096+Fedmecucci@users.noreply.github.com>
2024-04-11 11:10:32 +0200

    Add files via upload

---
+++ src/diadia/Giocatore.java
@@ -0,0 +1,27 @@
+package diadia;
+
+public class Giocatore {
+
+	
+	static final private int CFU_INIZIALI = 20;
+	private Borsa borsa;
+	private int cfu;
+	
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+	
+	public Borsa getBorsa() {
+		return borsa;
+	}
+	public int getCfu() {
+		return cfu;
+	}
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+}
